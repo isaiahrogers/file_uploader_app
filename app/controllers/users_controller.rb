3879eac5a770f7e8da5074483c4cb672d036b4f8
Isaiah Rogers <isaiah@mortarstone.com>
2018-12-15 16:33:25 -0800

    Implement bcrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,25 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = 'New user created'
+    else
+      flash[:notice] = 'Issue creating user'
+      render 'new'
+    end
   end
 
   def index
   end
 
   def show
+    @current_user = User.find(params[:id])
   end
 
   def edit
@@ -16,6 +29,12 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 2018_12_15_233619) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
